9902ae55587b60ffa18c680439b70946781d0b3c
Epaphras Kyomnom Peter <epaphraspeter76@gmail.com>
2024-07-26 10:25:02 +0100

    initial commit

---
+++ indes.js
@@ -0,0 +1,24 @@
+// Creating an Array
+// // let users = []; // an empty array.
+// let users = ["Epaphras", "John", "Peter"];  
+// let items = ["Epaphras", 20, 30.5, { themes: "dark"}]; // having multiple items in an array including an object
+let animals =["Dog" , "Goat" , "Monkey" , "Sheep" , "Cat"]
+// alert(animals);
+
+// // animals[4]= "Horse";    //to update the value on an item in an array
+// // console.log(animals[4]);
+// alert(animals);
+// console.log(items);
+
+// Array Methods
+// animals.push("pig"); // To add an item to the end of the array
+// animals.pop(); // To remove an item from the end of the array
+// console.log(animals);
+
+// animals.shift(); // Removes an item from the beginning of an array
+// // console.log(animals)
+
+// animals.unshift("cheeta"); // Adds an item to the beginning of an array
+// console.log(animals)
+
+console.log(animals.indexOf('Sheep'));
